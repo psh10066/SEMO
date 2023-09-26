1e0b9dae94b8bc178549ef7db9dcda75316bddf6
Suibbsu <gkdlfngoflvh@naver.com>
2023-09-26 10:03:45 +0900

    front back 생성

---
+++ semo_back/src/main/java/kr/or/semo/SemoBackApplication.java
@@ -0,0 +1,13 @@
+package kr.or.semo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemoBackApplication.class, args);
+	}
+
+}
---
+++ semo_back/src/test/java/kr/or/semo/SemoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.semo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
