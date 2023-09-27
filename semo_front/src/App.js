2899a766fca42ccdd0b0646480cd916622527d2f
JeongahHan <142190043+JeongahHan@users.noreply.github.com>
2023-09-27 11:40:23 +0900

    fix : 헤더 푸터 수정

--- semo_front/src/App.js
+++ semo_front/src/App.js
@@ -1,12 +1,15 @@
+import Footer from "./component/common/Footer";
 import Header from "./component/common/Header";
 import "./component/common/default.css";
 
 function App() {
   return (
     <div className="wrap">
+      <Header />
       <div className="content">
-        <Header />
+        <></>
       </div>
+      <Footer />
     </div>
   );
 }
