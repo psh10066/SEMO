64a220b5e825aec2ff1bfb65f6c26df1c7472ae0
yoomin <over100s@naver.com>
2023-10-04 17:18:15 +0900

    feat : mapper추가

--- semo_back/src/main/java/kr/or/semo/group/model/service/GroupService.java
+++ semo_back/src/main/java/kr/or/semo/group/model/service/GroupService.java
@@ -6,19 +6,21 @@ import org.springframework.transaction.annotation.Transactional;
 
 import kr.or.semo.group.model.dao.GroupDao;
 import kr.or.semo.group.model.vo.Group;
+import kr.or.semo.member.model.dao.MemberDao;
+
 
 @Service
 public class GroupService {
 	@Autowired
 	private GroupDao groupDao;
+	@Autowired
+	private MemberDao memberDao;
 	
 	//모임생성
 	@Transactional
 	public int insertGroup(Group g) {
 		int result = groupDao.insertGroup(g);
-		if(result>0) {
-		}
-		return 0;
+		return result;
 	}
 
 }
