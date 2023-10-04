55be049374f4bcb8182fb25d33e765f607d5b35d
JeongahHan <142190043+JeongahHan@users.noreply.github.com>
2023-10-04 17:36:37 +0900

    fix : 헤더 사라지는 문제 해결

--- semo_front/src/component/mainpage/MainPage.js
+++ semo_front/src/component/mainpage/MainPage.js
@@ -1,14 +1,20 @@
 import "./circle.css";
 import { Experience } from "./Experience";
 import { Canvas } from "@react-three/fiber";
+import MainSearch from "./search/MainSearch";
 
 const Mainpage = () => {
   return (
-    <div className="circle">
-      <Canvas shadows camera={{ position: [0, 16, 42], fov: 30 }}>
-        <Experience />
-      </Canvas>
-    </div>
+    <>
+      <div className="circle">
+        <Canvas shadows camera={{ position: [0, 16, 42], fov: 30 }}>
+          <Experience />
+        </Canvas>
+      </div>
+      <div className="mainSearch">
+        <MainSearch />
+      </div>
+    </>
   );
 };
 
