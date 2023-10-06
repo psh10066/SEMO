78e0ce11c9da5c7174dffaff49e47cdb20763ae4
temporaryUsernameCHOI <142180866+temporaryUsernameCHOI@users.noreply.github.com>
2023-10-06 18:35:22 +0900

    feat: 공지사항 작성 시 quill 에디터 사용 기능 추가

--- semo_front/src/component/notice/NoticeFrm.js
+++ semo_front/src/component/notice/NoticeFrm.js
@@ -1,4 +1,7 @@
 import "./notice.css";
+import Input from "../util/InputFrm";
+import NoticeTextEditor from "../util/NoticeTextEditor";
+import { Button2 } from "../util/Buttons";
 
 const NoticeFrm = (props) => {
   const noticeTitle = props.noticeTitle;
@@ -6,10 +9,49 @@ const NoticeFrm = (props) => {
   const noticeContent = props.noticeContent;
   const setNoticeContent = props.setNoticeContent;
 
+  const buttonEvent = props.buttonEvent;
+  const type = props.type;
+
   return (
     <div className="notice-frm-wrap">
-      <div className="notice-frm-top">
-        <div className="notice-title"></div>
+      <table className="notice-tbl">
+        <tbody>
+          <tr>
+            <td>
+              <label htmlFor="noticeTitle">제목</label>
+            </td>
+            <td>
+              <Input
+                type="text"
+                data={noticeTitle}
+                setData={setNoticeTitle}
+                content="noticeTitle" //상단 라벨과 연결
+              />
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="2">
+              <textarea
+                onChange={(e) => {
+                  const changeValue = e.currentTarget.value;
+                  setNoticeContent(changeValue);
+                }}
+              >
+                {noticeContent}
+              </textarea>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="notice-content-box">
+        <NoticeTextEditor data={noticeContent} setData={setNoticeContent} />
+      </div>
+      <div className="notice-btn-box">
+        {type === "modify" ? (
+          <Button2 text="수정하기" clickEvent={buttonEvent} />
+        ) : (
+          <Button2 text="작성하기" clickEvent={buttonEvent} />
+        )}
       </div>
     </div>
   );
---
+++ semo_front/src/component/util/NoticeTextEditor.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { useMemo, useRef } from "react";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import ImageResize from "quill-image-resize-module-react";
+Quill.register("modules/ImageResize", ImageResize);
+const NoticeTextEditor = (props) => {
+  const quillRef = useRef();
+  const data = props.data;
+  const setData = props.setData;
+  const url = props.url;
+
+  //quill에디터 형식옵션을 담는 배열
+  const formats = [
+    "header",
+    "font",
+    "side",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "align",
+    "image",
+    "color",
+  ];
+  //useMemo : 동일값을 반환하는경우 함수를 반복적으로 호출하는 것이 아니라 메모리에 저장해두고
+  //바로 가져오는 hooks
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        //툴바에 넣을 기능을 순서대로 나열
+        container: [
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ size: ["small", false, "large", "huge"] }, { color: [] }],
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+            { align: [] },
+          ],
+        ],
+      },
+    };
+  }, []);
+  return (
+    <ReactQuill
+      ref={quillRef}
+      theme="snow"
+      value={data}
+      formats={formats}
+      onChange={setData}
+      modules={modules}
+    />
+  );
+};
+
+export default NoticeTextEditor;
