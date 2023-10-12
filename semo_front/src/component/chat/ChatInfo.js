91c3af348f04cc9d9ebf7b1906ed6a039476e991
JeongahHan <142190043+JeongahHan@users.noreply.github.com>
2023-10-12 18:19:30 +0900

    feat : 채팅 구현 1

---
+++ semo_front/src/component/chat/ChatInfo.js
@@ -0,0 +1,14 @@
+import "./chat.css";
+
+const ChatInfo = () => {
+  return (
+    <div className="chat-room-info">
+      <p className="chat-room-info-text">
+        세모모 채팅에 오신걸 환영합니다!<br></br>
+        당신의 모임에 클릭하세요!
+      </p>
+    </div>
+  );
+};
+
+export default ChatInfo;
