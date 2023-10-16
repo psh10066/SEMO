66433ab4a437f2e05df878d027312d4d867bce66
JeongahHan <142190043+JeongahHan@users.noreply.github.com>
2023-10-16 19:02:51 +0900

    fix : 채팅 기능 수정

---
+++ semo_back/src/main/java/kr/or/semo/chatserver/service/ChatServerService.java
@@ -0,0 +1,29 @@
+package kr.or.semo.chatserver.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.or.semo.chatserver.model.dao.ChatServerDao;
+import kr.or.semo.chatserver.model.vo.LastAccessChatTime;
+import kr.or.semo.chatserver.model.vo.Message;
+
+@Service
+public class ChatServerService {
+	@Autowired
+	private ChatServerDao chatServerDao;
+
+	@Transactional 
+	public int messageInsert(Message messagePayload) {
+		return chatServerDao.messageInsert(messagePayload);
+	}
+	
+	@Transactional
+	public int chatAccessTimeInsert(LastAccessChatTime lastAccessChatTime) {
+		return chatServerDao.chatAccessTimeInsert(lastAccessChatTime);
+	}
+
+
+}
--- semo_front/src/component/chat/ChatRoom.js
+++ semo_front/src/component/chat/ChatRoom.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Client } from "@stomp/stompjs";
+import { format } from "date-fns";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ChatRoom = (props) => {
-  const { roomId, senderName } = props;
+  const { roomId, senderName, memberNo } = props;
   const [messages, setMessages] = useState({});
   const [newMessage, setNewMessage] = useState("");
   const clientRef = useRef(null);
 
+  //메세지 받기
   useEffect(() => {
     const client = new Client({
       brokerURL: "ws://localhost:9999/ws",
@@ -27,14 +31,44 @@ const ChatRoom = (props) => {
       console.error("STOMP error", frame);
     };
 
+    //
+    //roomId 가 바뀌면  current clinet active  / 이전 client 웹소켓 종료
     client.activate();
     clientRef.current = client;
 
     return () => {
+      sendLastAccessTime(); // 웹소켓 종료 전 마지막 접속 시간 전송
       client.deactivate();
     };
   }, [roomId]);
 
+  //마지막 접속 시간 전송 함수
+  const sendLastAccessTime = () => {
+    const now = new Date();
+    const formattedDate = format(now, "yyyy-MM-dd HH:mm:ss");
+    const token = window.localStorage.getItem("token");
+    const lastAccessTime = {
+      roomId: roomId,
+      memberNo: memberNo,
+      chatMyLastTime: formattedDate,
+    };
+    axios
+      .post("/chat/insertAccessTime", lastAccessTime, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        console.log(roomId + "번 마지막 접속시간 저장 성공");
+      })
+      .catch((res) => {
+        console.log(res.response.status);
+      });
+  };
+
+  //
+  //
+  //화면에 메세지 띄우기
   const onMessageReceive = (msg, index) => {
     setMessages((prevMessages) => {
       const currentRoomMessages = prevMessages[roomId] || [];
@@ -45,18 +79,39 @@ const ChatRoom = (props) => {
     });
   };
 
+  //메세지 전송
   const sendMessage = () => {
     if (clientRef.current && newMessage) {
+      const now = new Date();
+      const formattedDate = format(now, "yyyy-MM-dd HH:mm:ss");
+      const token = window.localStorage.getItem("token");
+
       const messagePayload = {
         message: newMessage,
         senderName: senderName,
         roomId: roomId,
+        memberNo: memberNo,
+        chatTime: formattedDate,
       };
       clientRef.current.publish({
         destination: "/app/chat/" + roomId,
-        body: JSON.stringify(messagePayload),
+        body: JSON.stringify(messagePayload), //메세지 객체를 json 문자열로 바꿔줌
       });
-      setNewMessage("");
+      setNewMessage(""); // 메세지 보내고 입력창은 비워줌
+      //
+      //메세지는 전송되고 , 메세지 받기 전에  db에 채팅메세지 저장
+      axios
+        .post("/chat/insert", messagePayload, {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        })
+        .then((res) => {
+          console.log("메세지 저장 성공");
+        })
+        .catch((res) => {
+          console.log(res.response.status);
+        });
     }
   };
 
