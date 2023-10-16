1af7125d8575cea2cf056e664b15a50511f26ce4
lar <142170487+pondue@users.noreply.github.com>
2023-10-16 14:04:01 +0900

    feat : 피드 대댓글 수정

--- semo_front/src/component/util/Comment.js
+++ semo_front/src/component/util/Comment.js
@@ -13,7 +13,9 @@ const feedCommentRegist = (
   feedCommentNo2,
   isLogin,
   changeFeedComment,
-  setChangeFeedComment
+  setChangeFeedComment,
+  setFeedCommentContent,
+  setRecommentState
 ) => {
   const feedCommentInesrt = { feedNo, feedCommentContent, feedCommentNo2 };
   if (isLogin) {
@@ -28,6 +30,10 @@ const feedCommentRegist = (
         .then((res) => {
           if (res.data === 1) {
             setChangeFeedComment(!changeFeedComment);
+            setFeedCommentContent("");
+            if (setRecommentState) {
+              setRecommentState(true);
+            }
           }
         })
         .catch((res) => {
@@ -45,15 +51,13 @@ const feedCommentRegist = (
 const Comment = (props) => {
   const member = props.member;
   const isLogin = props.isLogin;
-  const feed = props.feed;
   const [feedCommentContent, setFeedCommentContent] = useState("");
   const [feedCommentNo2, setFeedCommentNo2] = useState(0);
   const [commentList, setCommentList] = useState([]);
   const [reCommentList, setReCommentList] = useState([]);
   const [feedReCommentContent, setFeedRecommentContent] = useState("");
-  const changeFeedComment = props.changeFeedComment;
-  const setChangeFeedComment = props.setChangeFeedComment;
-  const feedNo = feed.feedNo;
+  const [changeFeedComment, setChangeFeedComment] = useState([true]); //댓글 새로고침
+  const feedNo = props.feedNo;
 
   //댓글 작성하기
   const feedCommentSubmit = () => {
@@ -63,7 +67,9 @@ const Comment = (props) => {
       feedCommentNo2,
       isLogin,
       changeFeedComment,
-      setChangeFeedComment
+      setChangeFeedComment,
+      setFeedCommentContent,
+      null
     );
   };
   //댓글 리스트 불러오기
@@ -230,7 +236,9 @@ const CommentItem = (props) => {
       comment.feedCommentNo,
       isLogin,
       changeFeedComment,
-      setChangeFeedComment
+      setChangeFeedComment,
+      setFeedRecommentContent,
+      setRecommentState
     );
   };
   const navigate = useNavigate();
@@ -282,7 +290,7 @@ const CommentItem = (props) => {
             placeholder="댓글 추가..."
             ref={textRef}
             onInput={resizeHeight}
-            defaultValue={feedCommentContent}
+            value={feedCommentContent}
             id={feedCommentContent}
             onChange={(e) => {
               setFeedCommentContent(e.target.value);
@@ -448,7 +456,7 @@ const CommentContent = (props) => {
         placeholder="댓글 추가..."
         ref={textRef}
         onInput={resizeHeight}
-        defaultValue={commentContent}
+        value={commentContent}
         id={commentContent}
         onChange={(e) => {
           setCommentContent(e.target.value);
