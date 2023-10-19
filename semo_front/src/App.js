0a887fa72bb1ef009f54633b40c528c453ccbb86
Suibbsu <gkdlfngoflvh@naver.com>
2023-10-19 17:15:13 +0900

    fix. 이메일내용 변경

--- semo_front/src/App.js
+++ semo_front/src/App.js
@@ -23,7 +23,7 @@ import LoungeMain from "./component/lounge/LoungeMain";
 import MypageReport from "./component/mypage/MypageReport";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
@@ -33,7 +33,7 @@ function App() {
     } else {
       setIsLogin(true);
     }
-  }, []);
+  }, [isLogin]);
 
   return (
     <div className="wrap">
